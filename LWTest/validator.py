cfc1e3d68393010cce66e849adfd47bd403bb136
Charles Cognato <charlescognato@gmail.com>
2020-03-12 19:45:42 -0400

    More clean up.

--- LWTest/validator.py
+++ LWTest/validator.py
@@ -2,6 +2,7 @@ from typing import Callable
 
 import LWTest.LWTConstants as LWT
 
+
 _HIGH_MINS = (LWT.Tolerance.HIGH_VOLTAGE_MIN.value,
               LWT.Tolerance.HIGH_CURRENT_MIN.value,
               LWT.Tolerance.HIGH_POWER_MIN.value)
@@ -40,20 +41,25 @@ _SCALE_COLS = (LWT.TableColumn.SCALE_CURRENT.value,
 
 
 class Validator:
-    def __init__(self, passing: Callable, failing: Callable):
+    """Validate sensor readings."""
+
+    def __init__(self, passing: Callable, failing: Callable) -> None:
+        """passing: Function to call when reading passes.
+           failing: Function to call when reading fails."""
+
         self._passing = passing
         self._failing = failing
 
-    def validate_high_voltage_readings(self, readings: tuple):
+    def validate_high_voltage_readings(self, readings: tuple) -> None:
         self._validate_readings(_HIGH_MINS, _HIGH_MAXS, _HIGH_COLS, readings)
 
-    def validate_low_voltage_readings(self, readings: tuple):
+    def validate_low_voltage_readings(self, readings: tuple) -> None:
         self._validate_readings(_LOW_MINS, _LOW_MAXS, _LOW_COLS, readings)
 
-    def validate_scale_n_angle_readings(self, readings: tuple):
+    def validate_scale_n_angle_readings(self, readings: tuple) -> None:
         self._validate_readings(_SCALE_MINS, _SCALE_MAXS, _SCALE_COLS, readings)
 
-    def validate_temperature_readings(self, room_temperature: float, readings: tuple):
+    def validate_temperature_readings(self, room_temperature: float, readings: tuple) -> None:
         for index, temperature in enumerate(readings):
             if temperature == 'NA':
                 continue
@@ -62,7 +68,7 @@ class Validator:
             else:
                 self._passing(index, LWT.TableColumn.TEMPERATURE.value)
 
-    def _validate_readings(self, tol_min: tuple, tol_max: tuple, cols: tuple, readings: tuple):
+    def _validate_readings(self, tol_min: tuple, tol_max: tuple, cols: tuple, readings: tuple) -> None:
         for sensor_index, sensor_readings in enumerate(readings):
             if sensor_readings[0] == 'NA':
                 continue
@@ -72,9 +78,8 @@ class Validator:
             for index, reading in enumerate(readings_as_floats):
                 self._validate(readings_as_floats[index], tol_min[index], tol_max[index], sensor_index, cols[index])
 
-    def _validate(self, reading: float, min: float, max: float, row: int, column: int):
+    def _validate(self, reading: float, min: float, max: float, row: int, column: int) -> None:
         if min >= reading or max <= reading:
             self._failing(row, column)
         else:
             self._passing(row, column)
-

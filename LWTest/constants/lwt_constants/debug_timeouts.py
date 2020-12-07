d63047d79f7b636002405647a1da1e468225633c
Charles Cognato <charlescognato@gmail.com>
2020-12-07 17:54:11 -0500

    Got tired of waiting 20 seconds.

--- LWTest/constants/lwt_constants/debug_timeouts.py
+++ LWTest/constants/lwt_constants/debug_timeouts.py
@@ -7,7 +7,7 @@ class TimeOut(Enum):
     URL_READ_INTERVAL = 2
     CONFIRM_SERIAL_CONFIG = 20
     COLLECTOR_BOOT_WAIT_TIME = 25
-    LINK_CHECK = 20
+    LINK_CHECK = 13
     LINK_PAGE_LOAD_INTERVAL = 1  # time to sleep between successive loads of the modem status page
     WAIT_FOR_COLLECTOR_TO_START_UPDATING_LOG_FILE = 1
     UPGRADE_LOG_LOAD_INTERVAL = 0.1

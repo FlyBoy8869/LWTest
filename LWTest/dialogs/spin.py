ecf76ff850f35d04fe32f56ebdc8c69d3aac4312
Charles Cognato <charlescognato@gmail.com>
2020-12-07 14:39:27 -0500

    Refactored to remove unnecessary attribute.

--- LWTest/dialogs/spin.py
+++ LWTest/dialogs/spin.py
@@ -1,3 +1,5 @@
+import time
+
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QProgressBar
 
@@ -5,10 +7,8 @@ from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QProgressBar
 class SpinDialog(QDialog):
     def __init__(self, parent, message: str, timeout: int):
         super().__init__(parent=parent)
-        self.timeout = timeout
 
         self.main_layout = QVBoxLayout()
-
         self.main_layout.addWidget(QLabel(message))
 
         self.activity = QProgressBar()
@@ -22,13 +22,12 @@ class SpinDialog(QDialog):
 
         self.timer = QTimer(self)
         self.timer.timeout.connect(self._timer_timeout)
-
         if timeout:
-            QTimer.singleShot(1000, lambda: self.timer.start(1000))
+            QTimer.singleShot(500, lambda: self.timer.start(500))
+            self.end_time = time.time() + timeout
 
     def _timer_timeout(self):
-        self.timeout -= 1
-        if self.timeout < 1:
+        if time.time() >= self.end_time:
             self.timer.stop()
             self.accept()
 

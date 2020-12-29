fb7daca09d42f84ba716f9730a88eccbb20c7b99
Charles Cognato <charlescognato@gmail.com>
2020-12-28 19:25:00 -0500

    - DateTimeSynchronizer now returns the updated date string. - Created Power class to check if the collector is "powered"

--- LWTest/collector/state/state.py
+++ LWTest/collector/state/state.py
@@ -3,6 +3,9 @@ from datetime import datetime, timedelta
 
 from selenium import webdriver
 
+import LWTest.constants.lwt_constants as lwt_const
+from LWTest.collector.state.reachable import PageReachable
+
 _DATE_AND_TIME_ELEMENT = "//*[@id='maindiv']/div[2]"
 _DATE_AND_TIME_INPUT_ELEMENT = "//*[@id='maindiv']/form/input[1]"
 _ADMIN_PASSWORD_INPUT_ELEMENT = "//*[@id='maindiv']/form/input[2]"
@@ -11,6 +14,8 @@ _UPDATE_BUTTON = "//*[@id='maindiv']/form/input[3]"
 
 class DateTimeSynchronizer:
     """Ensures the collector date and time is within 1 minute of the current date and time."""
+    DELTA_THRESHOLD: float = 2.0
+
     def __init__(self, url: str, password: str):
         self._logger = logging.getLogger(__name__)
         self._url = url
@@ -19,15 +24,16 @@ class DateTimeSynchronizer:
     def sync_date_time(self, driver: webdriver.Chrome):
         driver.get(self._url)
         time_delta = self._get_delta_in_minutes(
-            datetime.now(), self._get_collector_date(driver, self._logger),
-            self._logger
+            datetime.now(), self._get_collector_date(driver, self._logger), self._logger
         )
 
-        if time_delta > 1.0:
-            self._set_date(self._password, driver, self._logger)
+        if time_delta > self.DELTA_THRESHOLD:
+            return self._set_date(self._password, driver, self._logger)
+
+        return None
 
     @staticmethod
-    def _set_date(password: str, driver: webdriver.Chrome, logger):
+    def _set_date(password: str, driver: webdriver.Chrome, logger) -> str:
         now = datetime.now()
         current_date_time_str = \
             f"{now.year}-{now.month:02d}-{now.day:02d} {now.hour:02d}:{now.minute:02d}:{now.second:02d}"
@@ -39,9 +45,15 @@ class DateTimeSynchronizer:
         driver.find_element_by_xpath(_ADMIN_PASSWORD_INPUT_ELEMENT).send_keys(password)
         driver.find_element_by_xpath(_UPDATE_BUTTON).click()
 
+        return current_date_time_str
+
     @staticmethod
-    def _get_delta_in_minutes(now: datetime, collector_date: datetime, logger) -> float:
-        delta = (now - collector_date) / timedelta(minutes=1)
+    def _get_delta_in_minutes(time_1: datetime, time_2: datetime, logger) -> float:
+        """Returns the difference of time_1 - time_2 in minutes."""
+        # Don't change this formula. It accounts for the possible difference
+        # of exact days i.e., where days is >= 1 and the hours, minutes and seconds = 0, without
+        # having to check the other attributes of the timedelta object.
+        delta = (time_1 - time_2) / timedelta(minutes=1)
         logger.debug(f"time delta = {delta}")
         return delta
 
@@ -51,3 +63,20 @@ class DateTimeSynchronizer:
         date_time = datetime.strptime(element.get_attribute("textContent").split('\n', 1)[0], "%c")
         logger.debug(f"date and time read from the collector = {date_time}")
         return date_time
+
+
+class Power:
+    URL: str = lwt_const.URL_DATE_TIME
+    ON: bool = True
+    OFF: bool = False
+
+    def __init__(self):
+        self.checker = PageReachable(self.URL)
+
+    @property
+    def is_on(self):
+        return self.ON if self.checker.try_to_load() == PageReachable.REACHED else self.OFF
+
+    @property
+    def is_off(self):
+        return not self.is_on

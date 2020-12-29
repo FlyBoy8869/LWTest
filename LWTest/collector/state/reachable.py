13613992760056e38addfafa22c099922c772e52
Charles Cognato <charlescognato@gmail.com>
2020-12-28 19:19:29 -0500

    Refactored to be cleaner.

---
+++ LWTest/collector/state/reachable.py
@@ -0,0 +1,27 @@
+import logging
+
+import requests
+from PyQt5.QtCore import QObject
+
+
+class PageReachable(QObject):
+    REACHED: bool = True
+    UNREACHABLE: bool = False
+
+    TIMEOUT = 10
+
+    def __init__(self, url: str):
+        super().__init__()
+        self._logger = logging.getLogger(__name__)
+        self._url: str = url
+
+    def try_to_load(self):
+        msg = f"collector failed to serve: '{self._url}'"
+        try:
+            if 200 == requests.get(self._url, timeout=self.TIMEOUT).status_code:
+                return self.REACHED
+        except requests.exceptions.RequestException:
+            msg = f"unable to reach collector: {self._url}"
+
+        self._logger.debug(msg)
+        return self.UNREACHABLE
